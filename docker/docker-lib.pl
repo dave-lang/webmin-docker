f2161e9b48b38a255c5d72257ade2258e1ed2b18
Dave Lang <dave@dave-lang.com>
2023-07-11 14:52:06 +1000

    Minimal start/stop/restart functionality
    Also add initial release action

--- docker/docker-lib.pl
+++ docker/docker-lib.pl
@@ -3,17 +3,38 @@ Libs for docker module
 =cut
 
 BEGIN { push(@INC, ".."); };
+
+use strict;
+use warnings;
 use WebminCore;
+use JSON::PP;
 
 init_config();
 
+sub get_status {
+    my $fail;
+    my $status = "{}";
+    my $code = execute_command('docker info --format "{{json .}}"', undef, \$status, \$fail, 0, 1);
+
+    if ($code != 0) {
+        return $fail;
+    }
+
+    my $json = decode_json($status);
+    if ($json->{ ServerErrors }) {
+        return $json->{ ServerErrors };
+    }
+
+    return 0, $json;
+}
+
 sub get_containers
 {
-    local $containers, $fail;
+    my ($containers, $fail);
     my $code = execute_command('docker container ls --all --format {{.ID}},{{.Names}},{{.Image}},{{.Status}}', undef, \$containers, \$fail, 0, 1);
 
     if ($code != 0) {
-        print $fail;
+        return $fail;
     }
 
     my @results;
@@ -28,51 +49,53 @@ sub get_containers
         });
     }
 
-    return @results;
+    return 0, @results;
 }
 
 sub get_stats
 {
-    my $containers;
-    my $fail;
+    my ($containers, $fail);
     my $code = execute_command('docker stats --all --no-stream --format "{{.ID}},{{.CPUPerc}},{{.MemPerc}}"', undef, \$containers, \$fail, 0, 1);
 
     if ($code != 0) {
-        print $fail;
+        return $fail;
     }
 
 #{"BlockIO":"0B / 0B","CPUPerc":"0.00%","Container":"344327f11366","ID":"344327f11366","MemPerc":"0.00%","MemUsage":"0B / 0B","Name":"objective_dhawan","NetIO":"0B / 0B","PIDs":"0"}
-    return $containers;
+    # return 0, $containers;
 
-    %results = ( );
+    my %results = ( );
     my @containers = split(/\n/, $containers);
 
     foreach my $u (@containers) {
         my ($id, $cpu, $mem) = split(/,/, $u);
-        $results{ $id } = {
-            'id' => $id,
-            'name' => $name,
-            'image' => $image
+        $results{$id} = {
+            'cpu' => $cpu,
+            'mem' => $mem
         };
+        # $results{$id} = $id;
     }
 
-    return %results;
+    return 0, %results;
 }
 
 sub start_container
 {
     my($container) = @_;
-    execute_command('docker start ' . $container, undef, \$containers, undef, 0, 1);
+    my ($output, $fail);
+    execute_command('docker start ' . $container, undef, \$output, \$fail, 0, 1);
 }
 
 sub stop_container
 {
     my($container) = @_;
-    execute_command('docker stop ' . $container, undef, \$containers, undef, 0, 1);
+    my ($output, $fail);
+    execute_command('docker stop ' . $container, undef, \$output, \$fail, 0, 1);
 }
 
 sub restart_container
 {
     my($container) = @_;
-    execute_command('docker restart ' . $container, undef, \$containers, undef, 0, 1);
+    my ($output, $fail);
+    execute_command('docker restart ' . $container, undef, \$output, \$fail, 0, 1);
 }
--- docker/index.cgi
+++ docker/index.cgi
@@ -1,35 +1,57 @@
 #!/usr/bin/perl
+# use strict;
+use warnings;
+use Data::Dumper;
 
 require 'docker-lib.pl';
 
+my %text;
+
 ui_print_header(undef, $text{'index_title'}, "", undef, 1, 1);
 
-my @containers = get_containers();
-my @stats = get_stats();
-my $num_containers = @containers;
-
-print ui_subheading($text{'index_containers_title'} . " (" . $num_containers . ")");
-print ui_columns_start(['name', 'label', 'running for', ' ', ' ', ' ' ]);
-
-foreach my $u (@containers) {
-    print ui_columns_row([
-        html_escape($u->{'name'}),
-        html_escape($u->{'image'}),
-        html_escape($u->{'status'}),
-        "<a href='command.cgi?c=start&container=" . urlize($u->{'name'}) . "'>Start</a>",
-        "<a href='command.cgi?c=stop&container=" . urlize($u->{'name'}) . "'>Stop</a>",
-        "<a href='command.cgi?c=restart&container=" . urlize($u->{'name'}) . "'>Restart</a>",
-    ]);
-
-    print ui_columns_row([
-        @stats[$u->{'id'}]
-    ]);
+my($status_fail, @status) = get_status();
+if ($status_fail) {
+    print ui_alert_box($fail, 'danger');
+} else {
+    print Dumper($status);
 }
-print ui_columns_end();
+
+
+my($fail, @containers) = get_containers();
+my($stat_fail, %stats) = get_stats();
+# print(get_status());
+
+print Dumper(\%stats);
+
+print ui_subheading($text{'index_containers_title'});
+if ($fail) {
+    print ui_alert_box($fail, 'danger');
+} else {
+    print ui_columns_start(['name', 'label', 'running for', ' ', ' ', ' ' ]);
+    foreach my $u (@containers) {
+        my $container_stats = \%stats{$u->{'id'}};
+        print ui_columns_row([
+            html_escape($u->{'name'}),
+            html_escape($u->{'image'}),
+            html_escape($u->{'status'}),
+            html_escape($container_stats->{'cpu'}),
+            html_escape($container_stats->{'mem'}),
+            "<a href='command.cgi?c=start&container=" . urlize($u->{'name'}) . "'>Start</a>",
+            "<a href='command.cgi?c=stop&container=" . urlize($u->{'name'}) . "'>Stop</a>",
+            "<a href='command.cgi?c=restart&container=" . urlize($u->{'name'}) . "'>Restart</a>",
+        ]);
+    }
+    print ui_columns_end();
+}
 
 print ui_hr();
 
 print ui_subheading($text{'index_stats_title'});
-#print @stats['4e707a2c7215'];
+
+if ($fail) {
+    print ui_alert_box($stat_fail, 'danger');
+} else {
+    print $stats, "T";
+}
 
 ui_print_footer("/", $text{'index'});
